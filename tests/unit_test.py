d636031595d871f6d721f15f5385b081d4868dc3
Marilyn Zhang <marilynzhang@google.com>
2020-07-24 23:04:20 -0400

    Test cases for multi-instance evaluation
    Tests edge cases where there are variable length lists or empty lists,
    as well as IOU thresholds and proper matching between proposed and gold
    boxes

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,19 +1,20 @@
+from modules import util
 import modules.benchmark_pipeline
 from modules.bounding_box import BoundingBox
 import pytest
 
-box_a = BoundingBox(20, 20, 29, 29)
-box_b = BoundingBox(0, 0, 24, 24)
-box_c = BoundingBox(1, 2, 3, 4)
-box_d = BoundingBox(2, 3, 4, 5)
-box_e = BoundingBox(3, 1, 4, 2)
-box_f = BoundingBox(2, 2, 3, 3)
-box_g = BoundingBox(2, 1, 3, 2)
+_BOX_A = BoundingBox(20, 20, 29, 29)
+_BOX_B = BoundingBox(0, 0, 24, 24)
+_BOX_C = BoundingBox(1, 2, 3, 4)
+_BOX_D = BoundingBox(2, 3, 4, 5)
+_BOX_E = BoundingBox(3, 1, 4, 2)
+_BOX_F = BoundingBox(2, 2, 3, 3)
+_BOX_G = BoundingBox(2, 1, 3, 2)
 # covers: no overlap, 1 box overlap, overlap an entire side, partial overlap
-bounding_box_tests = [(box_a, box_b, 25 / 700), (box_a, box_c, 0),
-                      (box_b, box_c, 9 / 625), (box_c, box_d, 4 / 14),
-                      (box_e, box_f, 1 / 7), (box_f, box_g, 2 / 6),
-                      (box_e, box_g, 2 / 6)]
+bounding_box_tests = [(_BOX_A, _BOX_B, 25 / 700), (_BOX_A, _BOX_C, 0),
+                      (_BOX_B, _BOX_C, 9 / 625), (_BOX_C, _BOX_D, 4 / 14),
+                      (_BOX_E, _BOX_F, 1 / 7), (_BOX_F, _BOX_G, 2 / 6),
+                      (_BOX_E, _BOX_G, 2 / 6)]
 
 
 @pytest.mark.parametrize("box_1,box_2,expected", bounding_box_tests)
@@ -21,9 +22,39 @@ def test_iou(box_1, box_2, expected):
   assert box_1.calculate_iou(box_2) == expected
 
 
+box_list_1 = [[_BOX_A, _BOX_B], [_BOX_C, _BOX_D]]
+box_list_2 = [[_BOX_B, _BOX_A], [_BOX_D, _BOX_C]]
+box_list_3 = [[_BOX_A], [_BOX_C]]
+box_list_4 = [[_BOX_E], [_BOX_F]]
+box_list_5 = [[_BOX_G], [_BOX_G]]
+# Tests proper matching of proposed box to gold box, IOU thresholds
+# includes double penalty for false positive and false negative if
+# a proposed box doesn't match the IOU threshold and there exists a gold box
+# Also includes empty edge cases
+evaluation_tests = [(1, box_list_1, box_list_2, (1, 1, 1)),
+                    (1, box_list_1, box_list_3, (0.5, 0.5, 1)),
+                    (1, box_list_3, box_list_1, (0.5, 1, 0.5)),
+                    (1, [[]], [[]], (1, 1, 1)),
+                    (1, [[], []], box_list_3, (0, 1, 0)),
+                    (1, box_list_3, [[], []], (0, 0, 1)),
+                    (2 / 6, box_list_4, box_list_5, (1, 1, 1)),
+                    (1, box_list_3, box_list_5, (0, 0, 0))]
+
+
+@pytest.mark.parametrize("iou_threshold,proposed_boxes,gold_boxes,expected",
+                         evaluation_tests)
+def test_evaluate_proposed_bounding_boxes(iou_threshold, proposed_boxes,
+                                          gold_boxes, expected):
+  assert util.evaluate_proposed_bounding_boxes(iou_threshold, proposed_boxes,
+                                               gold_boxes) == expected
+
+
 def test_benchmark():
   find_icon_benchmark = modules.benchmark_pipeline.BenchmarkPipeline()
-  accuracy, avg_time_secs, avg_memory_mibs = find_icon_benchmark.evaluate()
+  accuracy, precision, recall, avg_time_secs, avg_memory_mibs = find_icon_benchmark.evaluate(
+  )
   assert avg_memory_mibs <= 1000
   assert avg_time_secs <= 60
   assert accuracy >= 0
+  assert precision >= 0
+  assert recall >= 0
