b1039ad1c0ed304e7a327ddca23776eda6390cdb
Marilyn Zhang <marilynzhang@google.com>
2020-08-13 23:29:48 +0000

    Move image and icon visualization to tooling branch

--- modules/analysis_util.py
+++ modules/analysis_util.py
@@ -58,22 +58,6 @@ def generate_histogram(samples: np.ndarray, title: str, xlabel: str,
   matplotlib.pyplot.close(fig=fig)
 
 
-def save_icon_with_image(icon: np.ndarray, image: np.ndarray, filename: str):
-  """Save icon and image side by side.
-
-  Arguments:
-      icon: numpy array representing the icon
-      image: numpy array representing the UI image
-      filename: filename for where to save the icon and image.
-  """
-  fig, ax = matplotlib.pyplot.subplots(figsize=(20, 10))
-  ax.imshow(image)
-  ax.axis("off")
-  fig.figimage(icon, 0, 0)
-  matplotlib.pyplot.savefig(filename, bbox_inches="tight", pad_inches=0.5)
-  matplotlib.pyplot.close(fig=fig)
-
-
 def generate_scatterplot(x: np.ndarray,
                          y: np.ndarray,
                          title: str,
