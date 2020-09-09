aaee0aa449cbfb6ce57a4ad5da4c053db8218386
Marilyn Zhang <marilynzhang@google.com>
2020-09-09 21:04:23 +0000

    Memory Calculation Updates
    Memory_profiler's memory_usage invoked with a function passed into it
    actually does not return the memory used by that function directly
    (contrary to what their documentation implies). Rather, it's the memory
    used by the entire current process in the duration of the function, in .1s intervals. Hence, to find the
    memory used by one function, we configured the memory calculation to be
    the maximum memory used during the function's execution subtracted by
    the memory used right before executing the function.

--- modules/benchmark_pipeline.py
+++ modules/benchmark_pipeline.py
@@ -3,6 +3,7 @@
 import argparse
 from typing import Tuple
 
+
 import cv2
 from modules import analysis_util
 from modules import defaults
@@ -134,7 +135,12 @@ class BenchmarkPipeline:
         self.image_clusters.append(image_contour_clusters)
         self.icon_contours.append(icon_contour)
       times.append(timer.calculate_latency_info(output_path))
-    print("Average time per image: %f" % np.mean(times))
+    time_info = "Average time per image: %f\n" % np.mean(times)
+    time_info += "Median time of images: %f" % np.median(times)
+    if output_path:
+      with open(output_path, "a") as output_file:
+        output_file.write(time_info)
+    print(time_info)
     return np.mean(times)
 
   def calculate_memory(self, icon_finder_object, output_path: str) -> float:
@@ -168,7 +174,12 @@ class BenchmarkPipeline:
         self.proposed_boxes.append(bboxes)
         self.image_clusters.append(image_contour_clusters)
         self.icon_contours.append(icon_contour)
-    print("Average MiBs per image: %f" % np.mean(mems))
+    memory_info = "Average MiBs per image: %f" % np.mean(mems)
+    memory_info += "Median MiBs per image: %f" % np.median(mems)
+    if output_path:
+      with open(output_path, "a") as output_file:
+        output_file.write(memory_info)
+    print(memory_info)
     return np.mean(mems)
 
   def find_icons(
@@ -267,8 +278,9 @@ class BenchmarkPipeline:
       self.image_list, self.gold_boxes = analysis_util.scale_images_and_bboxes(
           self.image_list, self.gold_boxes, 5, 5)
 
-    avg_runtime_secs, avg_memory_mbs = self.find_icons(icon_finder_object,
-                                                       output_path, True, True)
+    avg_runtime_secs, avg_memory_mbs = self.find_icons(
+        icon_finder_object, output_path, False,
+        True)
     if visualize:
       self.visualize_bounding_boxes("images/" + icon_finder_option + "/" +
                                     icon_finder_option + "-visualized",
